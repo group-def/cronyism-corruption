075cb99612324cad11b546f9ce504be638578c4f
Dan Bunn <danbunn42@gmail.com>
2021-03-23 15:01:00 +0000

    did answers for quiz b

--- js-quiz/quiz-b.js
+++ js-quiz/quiz-b.js
@@ -189,62 +189,82 @@ const finalBoxText = document.getElementById('finalboxtext')
 // define dictionary of questions and answers
 const myQuestions = [
     {
-        question: "During the first few months of the pandemic the UK saw an ever-growing demand for Personal Protective Equipment. As a government Minister for the Department of Health, you have been tasked with supplying the NHS with PPE to help keep their vital staff members safe whilst treating those suffering from COVID-19. In order to supply this essential PPE to doctors and nurses throughout the country, do you:",
+        question: "By the end of 2020, how much money had been spent on the track and trace system? Was it:",
         answers: {
-            A: 'Choose a well-established company with experience producing and supplying PPE',
-            B: 'Accept offers of help from companies who have volunteered their services during the pandemic',
-            C: 'Utilise various local companies around the country in order to get the PPE to where it is needed as quickly as possible',
-            D: 'Buy 400,000 gowns from a Turkish T-Shirt manufacturer'
+            A: '£37 billion',
+            B: '£51 million',
+            C: '£22 billion',
+            D: '£82 billion'
         },
-        correctAnswer: 'D'
+        correctAnswer: 'C'
     },
     {
-        question: 'In order to help stop the spread of Coronavirus, the UK needs a track and trace system to notify people who have been in contact with the virus and advise them to stay at home. There are a few options available for this, what do you choose? a) use the system that is already being used in the NHS b) build a brand new system c) use the google/ apple track track product d) auction the rights to produce the app',
+        question: 'On average, how long did it take for track and trace to alert members of the public that they had been in contact with COVID-19? Was it:',
         answers: {
-            A: "Build upon the NHS' existing contact tracing system",
-            B: "Hire Dido Harding - Tory peer and wife of Tory MP - to lead the development of a new system",
-            C: "Hold an auction to determine who will develop the app through a competitive bidding process",
-            D: "Follow other countries in adopting the widely popylar Google or Apple models"
+            A: "24 hours",
+            B: "7 days",
+            C: "10 days",
+            D: "2 days"
         },
         correctAnswer: 'B'
     },
     {
-        question: "In line with the Track and Trace programme, data concerning the results of Coronavirus tests must be collected and stored in order to inform people of when they may have come into contact with the virus. In order to store these large quantities of data, do you:",
+        question: "In October 2020, it was revealed that large amounts of data concerning the results of COVID tests had been lost, due to human error involving the use of Microsoft Excel. Roughly how much data was lost?",
         answers: {
-            A: "Design a bespoke solution to store the data in the safest, most effective way",
-            B: "Use existing NHS databases and systems",
-            C: "Store the data on paper",
-            D: "Use Microsoft Excel"
+            A: "20,000 test results",
+            B: "5,000 test results",
+            C: "11,000 test results",
+            D: "16,000 test results"
         },
         correctAnswer: "D"
     },
     {
-        question: 'In order for the track and trace scheme to work, the vast majority of the UK population have relinquished control of their data, in order for it to be used to help combat the spread of the disease. With all this data now in your hands, what do you do with it? a) use it to for other public benefit b) respect GDPR and keep it private c) use it solely for track and trace and then delete it once finished d) sell to a US databroker',
+        question: 'In April 2020, amidst worldwide shortages of PPE, the UK government ordered 400,000 protective gowns from Turkey, which ultimately failed UK safety standards and had to be returned. What product did the company traditionally specialise in producing? Was it:',
         answers: {
-            A: "Sell the data to a US databroker",
-            B: "Respect GDPR regulations and citizens' right to privacy and keep the data private",
-            C: "Use the data solely for track and trace purposes and destroy the dataset once this has been accomplished",
-            D: "Use the data in other schemes to benefit the public"
+            A: "T-Shirts",
+            B: "Children's toys",
+            C: "Ventilators",
+            D: "Protective equipment"
         },
         correctAnswer: 'A'
     },
     {
-        question: "Due to the increased strain on the NHS and increased demand for medical supplies, as a result of the Coronavirus pandemic, the government must allocate contracts for the supply of these crucial services. In order to make sure that these contracts are allocated as efficiently as possible, do you: a) give the contracts to companies/ individuals who have donated to the convservative party? b) award the contract to the most suitable company? ",
+        question: "When allocating contracts for the supply of essential products and services throughout the pandemic, the UK government reportedly established a 'high-priority lane' for companies with political connections. How much more likely would you be to receive a contract if you were in the high-priority lane compared to normal channels?",
         answers: {
-            A: "Pick randomly",
-            B: "Prioritise companies with political connections and hold the majority of negotiations in closed channels",
-            C: "Carefully select companies based on their due diligence records and their suitability for the task",
-            D: "Put the contracts out to open tender and allow the free market to function effectively"
+            A: "Twice as likely",
+            B: "5 times more likely",
+            C: "10 times more likely",
+            D: "12 times more likely"
+        },
+        correctAnswer: "C"
+    },
+    {
+        question: "As of December 2020, there has been a total of £48 billion worth of contracts awarded to companies to provide much needed supplies and services relating to the pandemic. What percentage of these contracts were granted without competition?",
+        answers: {
+            A: "91%",
+            B: "29%",
+            C: "58%",
+            D: "67%"
+        },
+        correctAnswer: "D"
+    },
+    {
+        question: "While schools were closed due to the pandemic, many of the UK's most vulnerable families received food parcels intended to replace the previous scheme of weekly vouchers worth £15. The food parcels supposedly contained £30 worth of food, intended to last for 10 days, what was the estimated actual value of produce in the food parcels people received?",
+        answers: {
+            A: "£27.99",
+            B: "£5.22",
+            C: "£14.99",
+            D: "£11.22"
         },
         correctAnswer: "B"
     },
     {
-        question: "Due to schools and other child care services being closed due to Coronavirus, many of the most vulnerable members of society have been unable to access meals which they would normally depend on, with almost 2 million children going hungry in the UK in 2020. In order to prevent vulnerable children suffering, do you:",
+        question: "The £22bn 'world beating' test and trace app was released in September 2020, after several delays. It was reported that between September and February the app prevented 600,000 cases, but what percentage of total cases does this amount to?",
         answers: {
-            A: "Extend the existing voucher schemes to provide support for vulnerable families",
-            B: "Subsidise discount cards for people to use to buy food and essential items",
-            C: "Pay private contractors to supply food packages to vulnerable families",
-            D: "Provide hardship grants to vulnerable families so that they can afford to eat"
+            A: "52%",
+            B: "79%",
+            C: "15.7%",
+            D: "9.3%"
         },
         correctAnswer: "C"
     }
@@ -275,41 +295,61 @@ const answerCards = [
     {
         correct: {
             title: 'Correct!',
-            content: 'blah blah'
+            content: "Whilst the logic behind this decision remains unclear, the Government opted to use Microsoft Excel to store results from Covid tests, which had been analysed by commercial firms. Although the use of Excel is not inherently problematic, issues arose due to the use of an old file format - developers used the XLS format which dates back to 1987, rather than the newer XLSX format - which can only store 65,000 rows of data, compared to the newer format which can store over 1 million. This ultimately resulted in the loss of around 15,000 cases over the 8 day period in which the events took place - with almost 2,000 cases being missed each day. This costly mistake left experts baffled, with Professor John Crowcroft of the University of Cambridge claiming the government should have opted for a bespoke solution, as 'nobody would start with [XLS]'."
         },
         incorrect: {
             title: 'Incorrect!',
-            content: 'blah'
+            content: "Whilst the logic behind this decision remains unclear, the Government opted to use Microsoft Excel to store results from Covid tests, which had been analysed by commercial firms. Although the use of Excel is not inherently problematic, issues arose due to the use of an old file format - developers used the XLS format which dates back to 1987, rather than the newer XLSX format - which can only store 65,000 rows of data, compared to the newer format which can store over 1 million. This ultimately resulted in the loss of around 15,000 cases over the 8 day period in which the events took place - with almost 2,000 cases being missed each day. This costly mistake left experts baffled, with Professor John Crowcroft of the University of Cambridge claiming the government should have opted for a bespoke solution, as 'nobody would start with [XLS]'."
         }
     },
     {
         correct: {
             title: 'Correct!',
-            content: 'blah blah'
+            content: 'In April 2020, the UK government ordered a shipment of 400,000 gowns from a Turkish T-Shirt manufacturer in order to deal with the huge demand for Personal Protective Equipment for NHS staff. Unfortunately, the gowns failed to meet UK standards and had to be returned to the supplier, leaving vulnerable NHS staff still desperately short of PPE.'
         },
         incorrect: {
             title: 'Incorrect!',
-            content: 'blah'
+            content: 'In April 2020, the UK government ordered a shipment of 400,000 gowns from a Turkish T-Shirt manufacturer in order to deal with the huge demand for Personal Protective Equipment for NHS staff. Unfortunately, the gowns failed to meet UK standards and had to be returned to the supplier, leaving vulnerable NHS staff still desperately short of PPE.'
         }
     },
     {
         correct: {
             title: 'Correct!',
-            content: 'blah blah'
+            content: "Rather than going through a competitive tender process - where bid for the right to run a service or gain a contract - a report by the National Audit Office found that the British government prioritised companies with political connections when allocating contracts. Reportedly, the government established a 'high-priority lane' for companies who were referred by government officials and ministers, with suppliers in this lane being roughly ten times more likely to obtain contracts than those processed through ordinary channels. 
         },
         incorrect: {
             title: 'Incorrect!',
-            content: 'blah'
+            content: "Rather than going through a competitive tender process - where bid for the right to run a service or gain a contract - a report by the National Audit Office found that the British government prioritised companies with political connections when allocating contracts. Reportedly, the government established a 'high-priority lane' for companies who were referred by government officials and ministers, with suppliers in this lane being roughly ten times more likely to obtain contracts than those processed through ordinary channels."
         }
     },
     {
         correct: {
             title: 'Correct!',
-            content: 'blah blah'
+            content: "Of the £17.3 billion worth of contracts which were awarded, roughly 67% of these contracts were awarded without a competitive process, which has ultimately lead to contracts being granted to companies with little suitability to the task. Most notably, US jewellery company, Saiger, was granted £250 million worth of PPE contracts despite having no experience in supplying in PPE, which resulted in the Department for Health and Social Care overpaying by almost £2.50 per gown. According to the Good Law Project, the government received over 24,000 offers from 16,000 suppliers, many of whom had experience in providing PPE, yet three of the biggest beneficiaries of government contracts were companies specialising in jewellery (Saiger), pest control (Pestfix), and an opaque 'family office' owned through a tax haven (Ayanda)."
         },
         incorrect: {
             title: 'Incorrect!',
+            content: "Of the £17.3 billion worth of contracts which were awarded, roughly 67% of these contracts were awarded without a competitive process, which has ultimately lead to contracts being granted to companies with little suitability to the task. Most notably, US jewellery company, Saiger, was granted £250 million worth of PPE contracts despite having no experience in supplying in PPE, which resulted in the Department for Health and Social Care overpaying by almost £2.50 per gown. According to the Good Law Project, the government received over 24,000 offers from 16,000 suppliers, many of whom had experience in providing PPE, yet three of the biggest beneficiaries of government contracts were companies specialising in jewellery (Saiger), pest control (Pestfix), and an opaque 'family office' owned through a tax haven (Ayanda)."
+        }
+    },
+    {
+        correct: {
+            title: 'Correct!',
+            content: "While some of the UK’s most vulnerable children went hungry due to COVID-19, the UK government replaced their existing voucher scheme - which gave vulnerable families £15 per week per child whilst schools were closed - with food parcels intended to last the whole week. Whilst the food packages were supposed to last 10 days and contain food worth around £30, in reality the produce in the packages was sparse and lacking in nutritional value, with the actual value of each parcel coming in at less than £10. The government paid £208 million to private firm, Chartwells, to carry out the scheme, with 4,724,611 boxes being distributed in England alone. Whilst there has been no comment on the cost of each individual box, based on the total cost of the scheme and the total number of boxes delivered, the New Statesman have estimated that each box cost the taxpayer over £44, despite their underwhelming contents."
+        },
+        incorrect: {
+            title: 'Incorrect!',
+            content: "While some of the UK’s most vulnerable children went hungry due to COVID-19, the UK government replaced their existing voucher scheme - which gave vulnerable families £15 per week per child whilst schools were closed - with food parcels intended to last the whole week. Whilst the food packages were supposed to last 10 days and contain food worth around £30, in reality the produce in the packages was sparse and lacking in nutritional value, with the actual value of each parcel coming in at less than £10. The government paid £208 million to private firm, Chartwells, to carry out the scheme, with 4,724,611 boxes being distributed in England alone. Whilst there has been no comment on the cost of each individual box, based on the total cost of the scheme and the total number of boxes delivered, the New Statesman have estimated that each box cost the taxpayer over £44, despite their underwhelming contents."
+        }
+    },
+    {
+        correct: {
+            title: 'Correct!',
             content: 'blah'
+        },
+        incorrect: {
+            title: 'Incorrect!',
+            content: 'blah blah'
         }
     }
 ];
